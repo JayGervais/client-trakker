e53da2c8d23d366d4d3f80618b89851625a1c9de
Jay Gervais <mrjaygervais@gmail.com>
2019-07-05 20:09:57 -0600

    Added a summary

--- ClientTrakker/MainWindow.xaml.cs
+++ ClientTrakker/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ using System.Data;
 namespace ClientTrakker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// An application used to track clients
     /// </summary>
     public partial class MainWindow : Window
     {
